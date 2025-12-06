bc08d3a4597c615a477d1cf3296153e4484f29a7
williammartins.paiva <williammartins.paiva@gmail.com>
2025-12-06 13:57:38 +0000

    Atualizando trabalho

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -11,46 +11,48 @@ class Resultado extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Respostas",
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 203, 152, 1),
-            ),
-          ),
-
-          const SizedBox(height: 20),
-          ...respostas.map(
-            (resp) => Itens(
-              pergunta: resp['pergunta'],
-              resposta: resp['resposta'],
-            ),
-          ),
-
-          const SizedBox(height: 20),
-
-          // ✅ BOTÃO DE REINICIAR
-          ElevatedButton(
-            onPressed: reiniciar,
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(double.infinity, 40),
-              padding: const EdgeInsets.all(10),
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero,
+      child: SingleChildScrollView(   // ✅ ADICIONADO
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Respostas",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 203, 152, 1),
               ),
             ),
-            child: const Text(
-              'Reiniciar',
-              style: TextStyle(fontSize: 25),
+
+            const SizedBox(height: 20),
+
+            ...respostas.map(
+              (resp) => Itens(
+                pergunta: resp['pergunta'],
+                resposta: resp['resposta'],
+              ),
             ),
-          ),
-        ],
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: reiniciar,
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 40),
+                padding: const EdgeInsets.all(10),
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+              ),
+              child: const Text(
+                'Reiniciar',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
